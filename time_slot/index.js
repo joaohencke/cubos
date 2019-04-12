61caec40b6abf800108a51a28378d079ad19a36a
Joao Hencke <joao@shiftcode.com.br>
2019-04-12 18:50:36 -0300

    timeslot controller

---
+++ time_slot/index.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const Boom = require('boom');
+
+let $data = [];
+
+const fsPromise = fs.promises;
+
+const reload = () => ($data = require('./persist/timeslot.json')); //eslint-disable-line
+
+const persist = () => fsPromise.writeFile(path.join(__dirname, 'persist/timeslot.json'), JSON.stringify($data));
+
+exports.find = ({ day } = {}) => {
+  reload();
+
+  return $data.filter(x => x.day === day);
+};
+
+exports.create = async ({ day, intervals, recurrence } = {}) => {
+  const result = exports.find({ day });
+
+  if (result.length)
+    throw Boom.badRequest('Já existe intervalo para o dia informado. Exclua-o para então, inseri-lo novamente');
+
+  $data.push({ day, intervals, recurrence });
+
+  await persist();
+
+  return $data[$data.length - 1];
+};
+
+exports.remove = async ({ day }) => {
+  const result = exports.find({ day });
+
+  if (!result.length) throw Boom.badRequest('Não existe intervalo para o dia cadastrado');
+
+  const index = $data.findIndex(x => x.day === day);
+
+  $data.splice(index, 1);
+
+  await persist();
+
+  return true;
+};
--- time_slot/router/time_slot.write.js
+++ time_slot/router/time_slot.write.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const controller = require('../');
 
 const router = express.Router({ mergeParams: true });
 
 module.exports = router;
 
-router.post('/', (req, res, next) => { });
+router.post('/', (req, res, next) => {});
 
-router.delete('/:date', (req, res, next) => { });
+router.delete('/:date', (req, res, next) => {});
