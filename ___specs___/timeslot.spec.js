dd7167cdbb7c271f26a243e3ff791a1f403ee843
Joao Hencke <joao.hencke@gmail.com>
2019-04-13 18:58:46 -0300

    tests

--- ___specs___/timeslot.spec.js
+++ ___specs___/timeslot.spec.js
@@ -12,12 +12,14 @@ const { expect } = chai;
 
 const fsPromise = fs.promises;
 
+let existingEntity;
+
 describe('time slot', () => {
   before(async () => {
     return fsPromise.writeFile(path.join(__dirname, '../time_slot/persist/timeslot_test.json'), '[]');
   });
 
-  it('should return an empty array', () => {
+  it('should return an empty array', done => {
     chai
       .request(server)
       .get('/api/time-slot')
@@ -25,10 +27,11 @@ describe('time slot', () => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.empty;
+        done();
       });
   });
 
-  it('should create an interval with none recurrence', () => {
+  it('should create an interval with none recurrence', done => {
     chai
       .request(server)
       .post('/api/time-slot')
@@ -39,12 +42,30 @@ describe('time slot', () => {
         recurrence: 'none',
       })
       .end((err, res) => {
+        existingEntity = res.body;
         expect(res).to.have.status(201);
         expect(res).to.be.json;
+        done();
+      });
+  });
+  it('should create an interval with daily recurrence', done => {
+    chai
+      .request(server)
+      .post('/api/time-slot')
+      .type('json')
+      .send({
+        day: '08-10-2018',
+        intervals: [{ start: '01:35', end: '01:45' }, { start: '02:35', end: '02:45' }],
+        recurrence: 'daily',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        done();
       });
   });
 
-  it('should throw exception time conflict', () => {
+  it('should throw exception time conflict', done => {
     chai
       .request(server)
       .post('/api/time-slot')
@@ -57,10 +78,11 @@ describe('time slot', () => {
       .end((err, res) => {
         expect(res).to.have.status(400);
         expect(res).to.be.json;
+        done();
       });
   });
 
-  it('should throw an exception because of invalid date format', () => {
+  it('should throw an exception because of invalid date format', done => {
     chai
       .request(server)
       .post('/api/time-slot')
@@ -73,10 +95,11 @@ describe('time slot', () => {
       .end((err, res) => {
         expect(res).to.have.status(400);
         expect(res).to.be.json;
+        done();
       });
   });
 
-  it('should throw an exception because of invalid time format', () => {
+  it('should throw an exception because of invalid time format', done => {
     chai
       .request(server)
       .post('/api/time-slot')
@@ -89,6 +112,41 @@ describe('time slot', () => {
       .end((err, res) => {
         expect(res).to.have.status(400);
         expect(res).to.be.json;
+        done();
+      });
+  });
+
+  it('sould remove an existing entity', done => {
+    chai
+      .request(server)
+      .delete(`/api/time-slot/${existingEntity.id}`)
+      .end((err, res) => {
+        expect(res).to.have.status(204);
+        done();
+      });
+  });
+
+  it('should retrieve a list of avaiables times between a date', done => {
+    chai
+      .request(server)
+      .get('/api/time-slot/avaiables')
+      .query({ start: '08-10-2018', end: '12-10-2018' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.length(5);
+        done();
+      });
+  });
+
+  it('should retrieve a list of avaiables times between a date', done => {
+    chai
+      .request(server)
+      .get('/api/time-slot/avaiables')
+      .query({ start: '07-10-2018', end: '12-10-2018' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.length(5);
+        done();
       });
   });
 });
